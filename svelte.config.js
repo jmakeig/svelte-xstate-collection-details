1333d64b4983fb0109f7018f18b0e262110bb586
Justin Makeig <jmpublic+github@makeig.com>
2022-01-15 20:37:43 +0000

    Wires up $components alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,15 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					// these are the aliases and paths to them
+					$components: path.resolve('./src/components')
+				}
+			}
+		}
 	}
 };
 
