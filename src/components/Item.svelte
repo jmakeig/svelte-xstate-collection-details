879e61d1335a66fe017210fa75682a454bdac830
Justin Makeig <jmpublic+github@makeig.com>
2022-01-22 19:29:05 +0000

    Binds form to machine context

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -1,7 +1,24 @@
 <script>
+	import { log } from 'xstate/lib/actions';
+
 	export let item; // Actor
 </script>
 
 <h2>Item</h2>
 <pre>{JSON.stringify($item.context, null, 2)}</pre>
-<label for="name">Name</label> <input id="name" name="name" value={$item.context.item.name} />
+<form
+	on:submit|preventDefault={(event) => console.log('submit')}
+	on:input={(event) => {
+		console.log(event.currentTarget);
+		const it = event.currentTarget;
+		item.send('update', { item: { name: it.name.value, description: it.description.value } });
+	}}
+>
+	<div>
+		<label for="name">Name</label> <input id="name" name="name" value={$item.context.item.name} />
+	</div>
+	<div>
+		<label for="description">Description</label>
+		<textarea id="description" name="description">{$item.context.item.name}</textarea>
+	</div>
+</form>
