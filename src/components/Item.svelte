273fe26a6287ad9ea970c6aa1c38141819becabe
Justin Makeig <jmpublic+github@makeig.com>
2022-01-27 04:27:16 +0000

    Adds error display

--- src/components/Item.svelte
+++ src/components/Item.svelte
@@ -1,9 +1,11 @@
 <script>
+	import { matchesState, State } from 'xstate';
+
 	export let item; // Actor
 </script>
 
 <section style="outline: solid 1px red; padding: 0.5em;">
-	<h2>Item</h2>
+	<h2>Item: {JSON.stringify($item.state.value, null, 2)}</h2>
 	<form
 		on:submit|preventDefault={(event) => console.log('submit')}
 		on:input={(event) => {
@@ -12,6 +14,9 @@
 			item.send('update', { item: { name: it.name.value, description: it.description.value } });
 		}}
 	>
+		{#if $item.state.matches('initialized.validated.invalid')}
+			<output>Yo! There are some errors: {$item.state.context.error.length}</output>
+		{/if}
 		<div>
 			<label for="name">Name</label> <input id="name" name="name" value={$item.name} />
 		</div>
@@ -19,5 +24,12 @@
 			<label for="description">Description</label>
 			<textarea id="description" name="description">{$item.description}</textarea>
 		</div>
+		<button
+			type="submit"
+			disabled={!(
+				$item.state.matches('initialized.mutated.dirty') &&
+				$item.state.matches('initialized.validated.valid')
+			)}>Save</button
+		>
 	</form>
 </section>
