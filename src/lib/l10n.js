ce6ad1140759eeb50ba4857ccaa9920876a9f780
Justin Makeig <jmpublic+github@makeig.com>
2022-02-11 06:42:26 +0000

    s/locale/language/

--- src/lib/l10n.js
+++ src/lib/l10n.js
@@ -1,16 +1,16 @@
 /**
  * @param {object | string} message the localized message
  * @param {string} [fallback = ''] if not localized message exists
- * @param {string} [locale] defaults to `window.navigator.language`
+ * @param {string} [language ] defaults to `window.navigator.language`
  * @return {string}
- * @throws {ReferenceError} avoids coercing `undefined` if no message is found for `locale` and `fallback` is not a `string`
+ * @throws {ReferenceError} avoids coercing `undefined` if no message is found for `language ` and `fallback` is not a `string`
  */
-export function message(message, fallback = '', locale) {
+export function message(message, fallback = '', language) {
 	if ('string' === typeof message) return message;
-	locale = locale || window?.navigator?.language || 'en';
-	if (locale in message) return message[locale];
-	const lang = locale.split('-')[0];
+	language = language || window?.navigator?.language || 'en';
+	if (language in message) return message[language];
+	const lang = language.split('-')[0]; // language-region, e.g. en-US
 	if (lang in message) return message[lang];
 	if ('string' === typeof fallback) return fallback;
-	throw new ReferenceError(`No message exists for ${locale}`);
+	throw new ReferenceError(`No message exists for ${language}`);
 }
